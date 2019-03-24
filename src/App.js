356fb7eea98f4597902b00f6b16a5f1294624e8c
Muhammad Shahzad <muhammad.shahzad@teo-intl.com>
2019-03-24 20:04:42 +0500

    added state and a field

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import {hot} from "react-hot-loader";
 import "./App.css";
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {name: "saif"}
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
   
   render(){
-    let username = "saif";
-
-    var items = ["saif", "ubaid"];
-    //arrow function
-    items.map(el => console.log(el))
-
     return(
       <div className="App">
-        <input type="text" />
-        <h1> Welcome, {username}! to SSUCoders</h1>
+        <input type="text" value={this.state.name} onChange={this.handleChange} />
+        <h1> Welcome, {this.state.name}! to SSUCoders</h1>
       </div>
     );
   }
