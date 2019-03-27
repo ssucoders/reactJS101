2f31e8feea71eaa39d3736da889ec1d98a6d16bc
mUbaidAwan <uawan2385@gmail.com>
2019-03-27 21:29:24 +0500

    support for adding new tasks

--- src/components/taskmanager.js
+++ src/components/taskmanager.js
@@ -5,8 +5,31 @@ class TasksManager extends Component {
     constructor(props){
         super(props);
         this.state = {
-            tasks: ["task one", "task two"]
+            tasks: [],
+            value: ""
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleKeyDown = this.handleKeyDown.bind(this)
+        this.addTask = this.addTask.bind(this)
+    }
+
+    handleChange(e){
+        console.log(e.target.value)
+        this.setState({value:e.target.value})
+    }
+
+    handleKeyDown(e){
+        console.log(e.which)
+        if(e.which===13 && this.state.value.trim().length>0){
+            this.addTask()
+        }
+    }
+
+    addTask(){
+        this.setState({
+            tasks: [...this.state.tasks, this.state.value], 
+            value:""
+        })
     }
 
     render(){
@@ -14,6 +37,8 @@ class TasksManager extends Component {
         return (
             <div>
                 <h1>Tasks Manager ({tasks.length})</h1>
+                <input type="text" value={this.state.value} onKeyDown={this.handleKeyDown} onChange={this.handleChange} />
+                <button>+</button>
                 {
                     tasks.map((el, i)=> {
                         return (
