0b2a5b4ad994434cbe13b81bcdce163d9c067898
mUbaidAwan <uawan2385@gmail.com>
2019-03-28 22:07:48 +0500

    add tasks with status

--- src/components/task.js
+++ src/components/task.js
@@ -3,21 +3,19 @@ import React, { Component} from "react";
 class Task extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            done: false
-        }
-        this.handleClick = this.handleClick.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    handleClick(){
-        this.setState({done:!this.state.done})
+    handleChange(e){
+       // this.setState({done:!this.state.done})
+       this.props.onChange(e.target.checked, this.props["data-index"]);
     }
 
     render(){
         return (
-            <div className={this.state.done?"task-done":""}>
-                <input onClick={this.handleClick} type="checkbox" />
-                {this.props.title}
+            <div className={this.props.item.done?"task-done":""}>
+                <input checked={this.props.item.done} onChange={this.handleChange} type="checkbox" />
+                {this.props.item.title}
                 {/* <button>X</button> */}
             </div>
         )
--- src/components/taskmanager.js
+++ src/components/taskmanager.js
@@ -5,12 +5,13 @@ class TasksManager extends Component {
     constructor(props){
         super(props);
         this.state = {
-            tasks: [],
+            tasks: [{title:"a task", done:false}, {title: "another task", done: true}],
             value: ""
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleKeyDown = this.handleKeyDown.bind(this)
         this.addTask = this.addTask.bind(this)
+        this.updateTaskStatus = this.updateTaskStatus.bind(this)
     }
 
     handleChange(e){
@@ -25,27 +26,46 @@ class TasksManager extends Component {
         }
     }
 
+    updateTaskStatus(status, index){
+        console.log(status);
+        console.log(index);
+        this.setState((state)=>{
+            let updatedTasks = state.tasks.map((el, i)=>{
+                if(i === index){
+                    el.done = status
+                }
+                return el;
+            })
+            return {tasks: updatedTasks}
+        })
+
+
+     }
+
     addTask(){
         this.setState({
-            tasks: [...this.state.tasks, this.state.value], 
+            tasks: [...this.state.tasks, {title:this.state.value, done:false}], 
             value:""
         })
     }
 
     render(){
         let tasks = this.state.tasks;
+        let completedTasks = tasks.filter(el=>{
+            return (el.done==true)
+        })
+
+        let tasklist = tasks.map((el, i)=> {
+            return (
+                <Task key={i} data-index={i} item={el} onChange={this.updateTaskStatus}/>
+            )
+        });
         return (
             <div>
-                <h1>Tasks Manager ({tasks.length})</h1>
+                <h1>Tasks Manager ({completedTasks.length}/{tasks.length})</h1>
                 <input type="text" value={this.state.value} onKeyDown={this.handleKeyDown} onChange={this.handleChange} />
                 <button>+</button>
-                {
-                    tasks.map((el, i)=> {
-                        return (
-                            <Task key={i} title={el}/>
-                        )
-                    })
-                }
+                {tasks.length>0?tasklist: <p>No tasks, add some Tasks</p>}
             </div>
         )
     }
